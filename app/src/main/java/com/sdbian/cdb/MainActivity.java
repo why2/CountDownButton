ff261caf7c44dbe589676abc873dae30f4fead53
SDBian <18949865454@163.com>
2017-12-20 16:55:36 +0800

    upload to jcenter

--- app/src/main/java/com/sdbian/cdb/MainActivity.java
+++ app/src/main/java/com/sdbian/cdb/MainActivity.java
@@ -2,6 +2,9 @@ package com.sdbian.cdb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_count).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "测试", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- countdownButton/src/main/java/com/sdbian/cdbutton/CountDownButton.java
+++ countdownButton/src/main/java/com/sdbian/cdbutton/CountDownButton.java
@@ -18,11 +18,11 @@ import android.view.Gravity;
  * 在使用的
  */
 public class CountDownButton extends AppCompatButton {
-    private int totalTime;//总时间
-    private String countingText;  //倒计时过程时间后面显示内容
-    private String text;//默认文字
+    private int totalTime; //total time
+    private String countingText;//counting content
+    private String text;//default content
     private CountDownTimer countDownTimer;
-    private String unit;//时间单位，s或者秒
+    private String unit;//time unit
 
     public CountDownButton(Context context) {
         super(context);
@@ -40,7 +40,7 @@ public class CountDownButton extends AppCompatButton {
     }
 
     /**
-     * 初始化配置
+     * init style
      *
      * @param context
      * @param attrs
@@ -48,14 +48,14 @@ public class CountDownButton extends AppCompatButton {
     private void initStyle(Context context, AttributeSet attrs) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CountDownButton);
         if (typedArray != null) {
-            //默认60秒
+            //default 60 s
             totalTime = typedArray.getInt(R.styleable.CountDownButton_totalTime, 60) * 1000;
             countingText = typedArray.getString(R.styleable.CountDownButton_countingText);
-            //默认英文
+            //default english
             int countUnit = typedArray.getInt(R.styleable.CountDownButton_countUnit, 0);
             switch (countUnit) {
                 case 1:
-                    unit = "秒";
+                    unit = getResources().getString(R.string.time_unit);
                     break;
                 default:
                     unit = "s";
@@ -63,7 +63,6 @@ public class CountDownButton extends AppCompatButton {
             }
             typedArray.recycle();
             text = getText().toString();
-            //防止没有设置倒计时过程中显示的内容
             countingText = countingText == null ? text : countingText;
             setEnabled(true);
             setLines(1);
@@ -72,7 +71,7 @@ public class CountDownButton extends AppCompatButton {
     }
 
     /**
-     * 开始计时
+     * start count
      */
     private void startCount() {
         if (isEnabled()) {
@@ -80,7 +79,7 @@ public class CountDownButton extends AppCompatButton {
             countDownTimer = new CountDownTimer(totalTime, 1000) {
                 @Override
                 public void onTick(long millisUntilFinished) {
-                    setText(millisUntilFinished / 1000 + unit + "后" + countingText);
+                    setText(millisUntilFinished / 1000 + unit + getResources().getString(R.string.after) + countingText);
                 }
 
                 @Override
@@ -94,9 +93,9 @@ public class CountDownButton extends AppCompatButton {
 
 
     /**
-     * performClick()在view被点击的时候触发，
-     * 设置setOnClickListener后的的回调方法onClick，
-     * 也是在performClick中间执行的
+     * performClick() after motion click,
+     * <p>
+     * setOnClickListener callback method onClick in performClick()
      *
      * @return
      */
@@ -119,12 +118,12 @@ public class CountDownButton extends AppCompatButton {
     }
 
     /**
-     * 设置倒计时的总时间
+     * setting totalTime
      *
-     * @param totalTime 总时间，单位秒
+     * @param totalTime
      */
     public void setTotalTime(int totalTime) {
-        this.totalTime = totalTime*1000;
+        this.totalTime = totalTime * 1000;
     }
 
     public String getCountingText() {
@@ -132,7 +131,7 @@ public class CountDownButton extends AppCompatButton {
     }
 
     /**
-     * 设置倒计时过程中显示文字，**S后**，或者**秒后**
+     * setting counting content
      *
      * @param countingText
      */
